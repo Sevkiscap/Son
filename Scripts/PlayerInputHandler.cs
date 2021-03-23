d581d7015b5e7035d236c9f7150e7ba44a5a81ef
Sevkiscap <63850097+Sevkiscap@users.noreply.github.com>
2021-03-23 11:53:02 -0600

    Add files via upload

---
+++ Scripts/PlayerInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkyForce.Player
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        void Update()
+        {
+           
+            if (Input.GetMouseButton(0))
+            {
+                    Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition).SetZ(0);
+                    PlayerService.Instance.SetFighterJetTo(position);
+            }
+         
+        }
+    }
+}
---
+++ Scripts/PlayerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkyForce.Player
+{
+    public class PlayerModel
+    {
+       
+        public PlayerView JetPrefab{get {return jetPrefab; }}
+        
+        public float ReloadTime{ get{ return reloadTime; }}
+        public PlayerModel(PlayerScriptableObject jetProperties)
+        {
+            jetPrefab = jetProperties.JetPrefab;
+            reloadTime = jetProperties.ReloadTime;
+        }
+    }
+}
